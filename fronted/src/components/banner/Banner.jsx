92e5b8e5f5745f27de941fa7ebfe0d6023255c22
hanwei <qqpp0504@gmail.com>
2024-11-23 03:10:58 +0800

    Create Banner component

---
+++ fronted/src/components/banner/Banner.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+import Button from "../UI/Button.jsx";
+
+export default function Banner({
+  bannerImage,
+  title,
+  description,
+  buttonText,
+  link,
+}) {
+  return (
+    <div className="padding-large py-4">
+      <div className="h-[60vh] flex justify-center">
+        <img
+          src={bannerImage}
+          alt="Banner image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="my-8 flex flex-col justify-center items-center">
+        <h3 className="text-[3rem] font-900">{title}</h3>
+        <p>{description}</p>
+        <Link to={link}>
+          <Button>{buttonText}</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -1,27 +1,14 @@
-import { Link } from "react-router-dom";
-
-import bannerImage from "../assets/banner-image.jpg";
-import Button from "../components/UI/Button.jsx";
+import homeBannerImage from "../assets/home-banner-image.jpg";
+import Banner from "../components/banner/Banner.jsx";
 
 export default function Home() {
   return (
-    <div className="padding-large py-4">
-      <div className="h-[60vh] flex justify-center">
-        <img
-          src={bannerImage}
-          alt="Banner image"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="my-8 flex flex-col justify-center items-center">
-        <h3 className="text-[3rem] font-900">精挑細選，只為你獻上理想之選</h3>
-        <p>
-          我們篩選全球優質單品，結合實用與風格，讓每件商品都成為生活中的必備精品。
-        </p>
-        <Link to="/">
-          <Button>探索我們的推薦單品</Button>
-        </Link>
-      </div>
-    </div>
+    <Banner
+      bannerImage={homeBannerImage}
+      title="精挑細選，只為你獻上理想之選"
+      description="我們篩選全球優質單品，結合實用與風格，讓每件商品都成為生活中的必備精品。"
+      buttonText="探索我們的推薦單品"
+      link="/"
+    />
   );
 }
