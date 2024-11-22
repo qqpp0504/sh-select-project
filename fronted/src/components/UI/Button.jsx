4c7db17481437cd30e6078accb8df28bacac130e
hanwei <qqpp0504@gmail.com>
2024-11-22 08:02:07 +0800

    Add desctiption to Home page

---
+++ fronted/src/components/UI/Button.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line react/prop-types
+export default function Button({ children }) {
+  return (
+    <p className="my-6 px-4 py-2 bg-black text-white rounded-3xl">{children}</p>
+  );
+}
