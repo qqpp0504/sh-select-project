894c50b9f9785d95283e776c90507120f8334f03
hanwei <qqpp0504@gmail.com>
2024-11-23 13:07:23 +0800

    Add sliding effect to Products buttons

--- fronted/src/components/banner/Products.jsx
+++ fronted/src/components/banner/Products.jsx
@@ -1,25 +1,54 @@
 /* eslint-disable react/prop-types */
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+
 import Button from "../UI/Button.jsx";
 import Icon from "../UI/Icon.jsx";
 
 export default function Products({ products }) {
+  const scrollContainerRef = useRef(); // 使用 useRef 引用滾動容器
+
+  const scrollAmount = 400; // 每次滾動的距離
+
+  function handleScrollLeft() {
+    scrollContainerRef.current.scrollBy({
+      left: -scrollAmount,
+      behavior: "smooth",
+    });
+  }
+
+  function handleScrollRight() {
+    scrollContainerRef.current.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <section className="my-16">
       <div className="padding-large flex items-center justify-between">
         <h2 className="text-2xl font-500">精選</h2>
         <div>
-          <button className="bg-gray-200 p-[0.875rem] rounded-[50%] mr-[0.625rem] opacity-40">
+          <button
+            onClick={handleScrollLeft}
+            className="bg-gray-200 p-[0.875rem] rounded-[50%] mr-[0.625rem] opacity-40"
+          >
             <Icon type="left-arrow" />
           </button>
-          <button className="bg-gray-200 p-[0.875rem] rounded-[50%]">
+          <button
+            onClick={handleScrollRight}
+            className="bg-gray-200 p-[0.875rem] rounded-[50%]"
+          >
             <Icon type="right-arrow" />
           </button>
         </div>
       </div>
 
       <article className="mx-2 my-3">
-        <ul className="flex flex-row gap-3 overflow-x-auto scrollbar-thin pb-8 px-10">
+        <ul
+          ref={scrollContainerRef}
+          className="flex flex-row gap-3 overflow-x-auto scrollbar-thin pb-8 px-10"
+        >
           {products.map((product) => (
             <li
               key={product.title}
