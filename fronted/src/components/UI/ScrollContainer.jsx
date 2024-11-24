efd5c7cfe0320d9910f0f60165fc8a6c4545ab73
hanwei <qqpp0504@gmail.com>
2024-11-25 00:52:55 +0800

    Add ScrollContainer UI

---
+++ fronted/src/components/UI/ScrollContainer.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+
+import Icon from "../UI/Icon.jsx";
+
+export default function ScrollContainer({ title, children }) {
+  const scrollContainerRef = useRef(); // 使用 useRef 引用滾動容器
+
+  const scrollAmount = 400; // 每次滾動的距離
+
+  function handleScrollLeft() {
+    scrollContainerRef.current.scrollBy({
+      left: -scrollAmount,
+      behavior: "smooth",
+    });
+  }
+
+  function handleScrollRight() {
+    scrollContainerRef.current.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  }
+
+  return (
+    <section className="my-16">
+      <div className="padding-large flex items-center justify-between">
+        <h2 className="text-2xl font-500">{title}</h2>
+        <div>
+          <button
+            onClick={handleScrollLeft}
+            className="bg-gray-200 p-[0.875rem] rounded-[50%] mr-[0.625rem] opacity-40"
+          >
+            <Icon type="left-arrow" />
+          </button>
+          <button
+            onClick={handleScrollRight}
+            className="bg-gray-200 p-[0.875rem] rounded-[50%]"
+          >
+            <Icon type="right-arrow" />
+          </button>
+        </div>
+      </div>
+
+      <article className="mx-2 my-3">
+        <ul
+          ref={scrollContainerRef}
+          className="flex flex-row gap-3 overflow-x-auto scrollbar-thin pb-8 px-10"
+        >
+          {children}
+        </ul>
+      </article>
+    </section>
+  );
+}
--- fronted/src/components/banner/Products.jsx
+++ fronted/src/components/banner/Products.jsx
@@ -1,74 +1,30 @@
 /* eslint-disable react/prop-types */
-import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 import Button from "../UI/Button.jsx";
-import Icon from "../UI/Icon.jsx";
+import ScrollContainer from "../UI/ScrollContainer.jsx";
 
 export default function Products({ products }) {
-  const scrollContainerRef = useRef(); // 使用 useRef 引用滾動容器
-
-  const scrollAmount = 400; // 每次滾動的距離
-
-  function handleScrollLeft() {
-    scrollContainerRef.current.scrollBy({
-      left: -scrollAmount,
-      behavior: "smooth",
-    });
-  }
-
-  function handleScrollRight() {
-    scrollContainerRef.current.scrollBy({
-      left: scrollAmount,
-      behavior: "smooth",
-    });
-  }
-
   return (
-    <section className="my-16">
-      <div className="padding-large flex items-center justify-between">
-        <h2 className="text-2xl font-500">精選</h2>
-        <div>
-          <button
-            onClick={handleScrollLeft}
-            className="bg-gray-200 p-[0.875rem] rounded-[50%] mr-[0.625rem] opacity-40"
-          >
-            <Icon type="left-arrow" />
-          </button>
-          <button
-            onClick={handleScrollRight}
-            className="bg-gray-200 p-[0.875rem] rounded-[50%]"
-          >
-            <Icon type="right-arrow" />
-          </button>
-        </div>
-      </div>
-
-      <article className="mx-2 my-3">
-        <ul
-          ref={scrollContainerRef}
-          className="flex flex-row gap-3 overflow-x-auto scrollbar-thin pb-8 px-10"
+    <ScrollContainer title="精選">
+      {products.map((product) => (
+        <li
+          key={product.title}
+          className="w-[27rem] h-[34rem] relative flex-shrink-0"
         >
-          {products.map((product) => (
-            <li
-              key={product.title}
-              className="w-[27rem] h-[34rem] relative flex-shrink-0"
-            >
-              <img
-                src={`http://localhost:3000/${product.image}`}
-                alt={product.alt}
-                className="w-full h-full object-cover object-bottom"
-              />
-              <div className="absolute bottom-7 left-12">
-                <h2 className="text-white text-xl font-400">{product.title}</h2>
-                <Link to="/">
-                  <Button bgColor="white">立即選購</Button>
-                </Link>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </section>
+          <img
+            src={`http://localhost:3000/${product.image}`}
+            alt={product.alt}
+            className="w-full h-full object-cover object-bottom"
+          />
+          <div className="absolute bottom-7 left-12">
+            <h2 className="text-white text-xl font-400">{product.title}</h2>
+            <Link to="/">
+              <Button bgColor="white">立即選購</Button>
+            </Link>
+          </div>
+        </li>
+      ))}
+    </ScrollContainer>
   );
 }
