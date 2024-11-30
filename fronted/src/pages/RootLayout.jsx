ee9accbe50942e5c996db2d541dbfbf7b77d62d5
hanwei <qqpp0504@gmail.com>
2024-11-30 21:08:30 +0800

    Add ScrollToTop component to RootLayout

--- fronted/src/pages/RootLayout.jsx
+++ fronted/src/pages/RootLayout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/navigation/MainNavigation.jsx";
 import BannerBar from "../components/BannerBar.jsx";
 import Footer from "../components/Footer.jsx";
+import ScrollToTop from "../components/ScrollToTop.jsx";
 
 export default function RootLayout() {
   return (
     <>
+      <ScrollToTop />
       <MainNavigation />
       <main>
         <BannerBar />
