a33fb82cfc510fb642e44ca978c689e73514e74e
hanwei <qqpp0504@gmail.com>
2024-12-20 22:11:56 +0800

    Add localStorage to Redux

--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "./store/index.js";
+import { store, persistor } from "./store/index.js";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- fronted/src/store/index.js
+++ fronted/src/store/index.js
@@ -1,11 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import filterReducer from "./filter-slice.js";
 import modalReducer from "./modal-slice.js";
 import cartReducer from "./cart-slice.js";
 
-const store = configureStore({
-  reducer: { filter: filterReducer, modal: modalReducer, cart: cartReducer },
+// 設定 persist 配置
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["filter", "modal"], // 不持久化的 slice
+};
+
+// 使用 combineReducers 合併所有 reducers
+const rootReducer = combineReducers({
+  filter: filterReducer,
+  modal: modalReducer,
+  cart: cartReducer,
 });
 
-export { store };
+// 使用 persistReducer 包裝 rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// 創建 store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // 忽略 redux-persist 的 action types
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
+});
+
+// 創建 persistor
+const persistor = persistStore(store);
+
+export { store, persistor };
