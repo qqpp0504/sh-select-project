5f7111fad20281c708ea6fc6f8d65fd03985bda6
hanwei <qqpp0504@gmail.com>
2025-01-22 05:02:04 +0800

    Add a feature in RootLayout to reset the state when the route changes

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 import MainNavigation from "../components/navigation/MainNavigation.jsx";
 import Footer from "../components/Footer.jsx";
 import ScrollToTop from "../components/ScrollToTop.jsx";
+import { favoritesActions } from "../store/favorites-slice.js";
 
 export default function RootLayout() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    // 當路由改變時重置狀態
+    dispatch(favoritesActions.resetState());
+  }, [location.pathname, dispatch]);
+
   return (
     <>
       <ScrollToTop />
