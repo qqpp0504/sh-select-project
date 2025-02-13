63d200798f7eed868fd5aa828186d42d3b48ae00
hanwei <qqpp0504@gmail.com>
2025-02-13 08:32:56 +0800

    Control Swiper index based on button selection in ProductImages component

--- frontend/src/components/productDetail/ProductImages.jsx
+++ frontend/src/components/productDetail/ProductImages.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -21,6 +21,7 @@ export default function ProductImages({
     setPreviewImage(allImages[currentIndex]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentIndex]);
+  const swiperRef = useRef(null);
 
   function handleEnterNextImage() {
     setCurrentIndex((prev) => (prev + 1) % allImages.length);
@@ -34,6 +35,13 @@ export default function ProductImages({
     setPreviewImage(image);
     setCurrentIndex(index);
   }
+
+  // 當 currentIndex 變化時，自動切換 Swiper 到對應的 slide
+  useEffect(() => {
+    if (swiperRef.current) {
+      swiperRef.current.slideTo(currentIndex);
+    }
+  }, [currentIndex]);
 
   return (
     <div className="lg:flex lg:flex-row w-full lg:w-[40rem] lg:h-[43rem]">
@@ -84,19 +92,26 @@ export default function ProductImages({
         </div>
       </div>
 
-      <div className={`block lg:hidden w-full ${classes.swiper}`}>
+      <div className="block lg:hidden w-full">
         <Swiper
           loop
           pagination={true}
           modules={[Pagination]}
-          className="mySwiper w-full aspect-[3/4] bg-gray-100"
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          className={`mySwiper w-full aspect-[3/4] bg-gray-100 ${classes.swiper}`}
         >
           {allImages.map((image) => (
-            <SwiperSlide key={image} className="w-full mb-2 overflow-hidden">
+            <SwiperSlide key={image} className="mb-2 relative">
               <img
                 src={`http://localhost:3000/${image}`}
                 alt={product.alt}
-                className="w-full h-full object-cover"
+                className={`w-full object-cover absolute top-1/2 -translate-y-1/2 ${
+                  product.images.noBg && product.images.noBg.includes(image)
+                    ? undefined
+                    : "h-full"
+                }`}
               />
             </SwiperSlide>
           ))}
