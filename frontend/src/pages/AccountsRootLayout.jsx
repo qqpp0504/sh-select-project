c838a380e49d3a8b7c5479abd5077e60f5b3e707
hanwei <qqpp0504@gmail.com>
2025-02-20 14:48:35 +0800

    Add RWD to the Account Page

--- frontend/src/pages/AccountsRootLayout.jsx
+++ frontend/src/pages/AccountsRootLayout.jsx
@@ -4,7 +4,7 @@ import logo from "@/assets/logo.png";
 
 export default function AccountsRootLayout() {
   return (
-    <div className="max-w-[30rem] mx-auto">
+    <div className="px-8 sm:px-0 max-w-[30rem] mx-auto">
       <header className="my-10">
         <img src={logo} alt="SH-Select Logo" className="w-14" />
       </header>
